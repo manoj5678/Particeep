d27e1de6ce5a2ad65b454eb14729b983dad7c4ab
manoj5678 <58529539+manoj5678@users.noreply.github.com>
2021-01-24 18:49:14 +0100

    Redux

---
+++ patriTask/test/src/App.js
@@ -0,0 +1,14 @@
+import Header from './Componnts/Header/header'
+
+import Footer from './Componnts/Footer/footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ patriTask/test/src/Componnts/Footer/footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function footer() {
+ 
+    return(
+        <div className ="Footer">
+  
+        <div className="col-md-12">
+          <div className="footer p-3 mt-4 text-center bg-dark text-light">
+            Developed By:
+            <span className="text-warning font-weight-normal">
+              Manoj Reddy
+            </span>
+            , Using <i className="fab fa-react" /> React JS &amp; Redux JS
+            
+             
+          </div>
+        </div>
+
+    </div>
+ 
+    )
+}
+
+
+export default footer
